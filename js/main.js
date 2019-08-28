eaf23ab146d700ccc7aa02e14f6a75e56c601c5d
ateet <16ucs050@lnmiit.ac.in>
2019-08-29 02:54:23 +0530

    All js added

--- js/main.js
+++ js/main.js
@@ -5,11 +5,20 @@ const tabContentItems = document.querySelectorAll(".tab-content-item");
 function selectItem(e) {
   //Add border to current tab
   removeBorder();
+  removeShow();
   this.classList.add("tab-border");
+  //Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  //Add show Class
+  tabContentItem.classList.add("show");
 }
 
 function removeBorder() {
   tabItems.forEach(item => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove("show"));
 }
 
 tabItems.forEach(item => item.addEventListener("click", selectItem));
